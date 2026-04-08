1f9efdcec02e61d9cf6d3c824daf822834efccae
“uuyeong” <kangyouyung@naver.com>
2026-04-08 16:13:20 +0900

    서강대학교

--- 2학년/기초인공지능프로그래밍/ch2_20231508_01.py
+++
@@ -1,9 +0,0 @@
-num = 1002
-n1 = num // 1000
-n2 = num // 100 % 10
-n3 = num // 10 % 10
-n4 = num % 10
-
-print("Number ", num, ":", n1, "+", n2, "+", n3,"+", n4,"=",n1+n2+n3+n4)
-print("Average: ", (n1+n2+n3+n4)/4)
-
